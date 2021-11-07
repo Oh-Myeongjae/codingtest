c48e55a6ae08ddb45046d8d8078b02bc9dc200c4
Oh-Myeongjae <dhaudwo1@daum.net>
2021-11-07 23:16:47 +0900

    코딩테스트 연습

--- level1/두 개 뽑아서 더하기.js
+++ level1/두 개 뽑아서 더하기.js
@@ -5,3 +5,23 @@
 numbers의 길이는 2 이상 100 이하입니다.
 numbers의 모든 수는 0 이상 100 이하입니다.
 */
+function solution(numbers) {
+    var answer = [];
+    const func = (a,arr) => {
+        if(arr.length === 1){
+            if(!answer.includes(a+arr[0])){
+                answer.push(a+arr[0])
+            }
+            return
+        }
+        for(let el of arr){
+            if(!answer.includes(a+el)){
+                answer.push(a+el)
+            }            
+        }
+        return func(arr[0],arr.slice(1))
+    }
+    func(numbers[0],numbers.slice(1))
+    answer.sort((a,b)=>a-b)
+    return answer;
+}
