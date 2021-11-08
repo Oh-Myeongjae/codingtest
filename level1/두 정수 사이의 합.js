903a87ff645cb7c0105011b62cc31f56dea343cd
Oh-Myeongjae <dhaudwo1@daum.net>
2021-11-08 23:34:46 +0900

    코딩테스트 연습

--- level1/두 정수 사이의 합.js
+++ level1/두 정수 사이의 합.js
@@ -7,3 +7,13 @@ a와 b가 같은 경우는 둘 중 아무 수나 리턴하세요.
 a와 b는 -10,000,000 이상 10,000,000 이하인 정수입니다.
 a와 b의 대소관계는 정해져있지 않습니다.
 */
+
+function solution(a, b) {
+    var answer = 0;
+    let Max = Math.max(a,b)
+    let Min = Math.min(a,b)
+    for(let i=Min;i<=Max;i++){
+        answer += i
+    }
+    return answer;
+}
