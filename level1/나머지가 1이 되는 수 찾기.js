b869365d06f39bf702308817b9fc14f06a7077f1
Oh-Myeongjae <dhaudwo1@daum.net>
2021-11-11 23:30:21 +0900

    코딩테스트 연습

--- level1/나머지가 1이 되는 수 찾기.js
+++ level1/나머지가 1이 되는 수 찾기.js
@@ -8,3 +8,13 @@ n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를
 3 ≤ n ≤ 1,000,000
 
 */
+function solution(n) {
+    var answer = 0;
+    for(let i=2;i<n;i++){
+        if(n%i === 1){
+            answer = i
+            break;
+        }
+    }
+    return answer;
+}
