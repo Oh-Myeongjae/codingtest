c1b66d079bd48af3be88f586d18adba5d9d1b29b
Oh-Myeongjae <dhaudwo1@daum.net>
2021-11-12 22:46:43 +0900

    코딩테스트 연습

--- level1/같은 숫자는 싫어.js
+++ level1/같은 숫자는 싫어.js
@@ -14,3 +14,15 @@ arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
 배열 arr의 크기 : 1,000,000 이하의 자연수
 배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
 */
+function solution(arr)
+{
+    var answer = [arr[0]];
+    
+    for(let el of arr){
+        if(answer[answer.length-1] !== el){
+            answer.push(el)
+        }
+    }
+    
+    return answer;
+}
