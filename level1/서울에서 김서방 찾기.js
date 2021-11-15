82b5c50fb34013c5a509895fd573326cf361c6fa
Oh-Myeongjae <dhaudwo1@daum.net>
2021-11-15 23:19:27 +0900

    코딩테스트 연습

--- level1/서울에서 김서방 찾기.js
+++ level1/서울에서 김서방 찾기.js
@@ -8,3 +8,8 @@ seoul은 길이 1 이상, 1000 이하인 배열입니다.
 seoul의 원소는 길이 1 이상, 20 이하인 문자열입니다.
 "Kim"은 반드시 seoul 안에 포함되어 있습니다.
 */
+function solution(seoul) {
+    let x = seoul.indexOf('Kim')
+    var answer = `김서방은 ${x}에 있다`;
+    return answer;
+}
