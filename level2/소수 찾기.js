f1e611a5eb3f36be4bbf1ac6c1d1bf3f7b10c174
Oh-Myeongjae <dhaudwo1@daum.net>
2021-11-24 23:31:44 +0900

    코딩테스트 연습

--- level2/소수 찾기.js
+++ level2/소수 찾기.js
@@ -11,3 +11,46 @@ numbers는 길이 1 이상 7 이하인 문자열입니다.
 numbers는 0~9까지 숫자만으로 이루어져 있습니다.
 "013"은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.
 */
+const func = (num)=>{
+    if(num <= 1)return false
+    for(let i=2;i<=Math.sqrt(num);i++){
+        if(num%i === 0){
+            return false
+        }
+    }
+    return true
+}
+
+function solution(numbers) {
+var answer = 0;
+let arr = []
+let list = []
+
+const listFunc = (now,char)=>{
+    if(now.length === 0){
+       if(!list.includes(char)){
+           list.push(char)
+       }
+        return
+    }
+    for(let i=0;i<now.length;i++){
+       let str1 = char + now[i]        
+       if(!list.includes(char)){
+           list.push(char)
+       }
+       let temp = now.slice(0,i).concat(now.slice(i+1))
+       listFunc(temp,str1)
+    }
+}
+listFunc(numbers.split(''),'')
+for(let el of list){
+    let num  = parseInt(el)
+    if(!isNaN(num) && !arr.includes(num)){
+        arr.push(num)
+    }
+}
+for(let el of arr){
+    if(func(el))answer++
+}
+return answer;
+}
