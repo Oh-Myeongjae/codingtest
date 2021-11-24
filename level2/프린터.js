e4b95a9a504bcdaad09cc3f258dabcda3faf85e8
Oh-Myeongjae <dhaudwo1@daum.net>
2021-11-24 23:30:41 +0900

    코딩테스트 연습

--- level2/프린터.js
+++ level2/프린터.js
@@ -20,3 +20,32 @@
 인쇄 작업의 중요도는 1~9로 표현하며 숫자가 클수록 중요하다는 뜻입니다.
 location은 0 이상 (현재 대기목록에 있는 작업 수 - 1) 이하의 값을 가지며 대기목록의 가장 앞에 있으면 0, 두 번째에 있으면 1로 표현합니다.
 */
+function solution(priorities, location) {
+    var answer = 0;
+    let now = location
+    while(now !== -1){
+        let temp = 0;
+        if(now === 0){
+            if(priorities[0] === Math.max(...priorities)){    
+                answer++
+                now--;
+            }else{
+                temp = priorities[0]
+                priorities.shift()
+                priorities.push(temp)
+                now = priorities.length-1
+            }
+        }else{
+            if(priorities[0] === Math.max(...priorities)){  
+                answer++
+                priorities.shift()
+            }else{
+                temp = priorities[0]
+                priorities.shift()
+                priorities.push(temp)
+            }
+             now--
+        }
+    }
+    return answer;
+}
