f0a1171d4d49d923be435f547f5b9654490f106d
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2022-01-04 22:57:37 +0900

    코딩테스트 연습

--- level2/숫자의 표현.js
+++ level2/숫자의 표현.js
@@ -12,3 +12,26 @@ Finn은 요즘 수학공부에 빠져 있습니다. 수학 공부를 하던 Finn
 제한사항
 n은 10,000 이하의 자연수 입니다.
 */
+function solution(n) {
+    var answer = 0;
+    let num = 0;
+    let last = 0;
+    for(let i=1;i<n;i++){
+        if(num+i<=n){
+            num += i
+            last = i
+            if(num === n)answer++
+        }else break;
+    }
+    let half = parseInt(n/2)
+    for(let i=2;i<=half;i++){
+        num -= (i-1)
+        if(num+(last+1)<=n){
+            num += last+1
+            last += 1
+            if(num === n)answer++
+        }
+    }
+    answer++
+    return answer;
+}
