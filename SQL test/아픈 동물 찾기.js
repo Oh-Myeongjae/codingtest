f83533962b0f7ed958fda930f5b4a44a1dc1ddd4
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2022-02-27 21:21:08 +0900

    코딩테스트 연습

--- SQL test/아픈 동물 찾기.js
+++ SQL test/아픈 동물 찾기.js
@@ -14,3 +14,5 @@ SEX_UPON_INTAKE	VARCHAR(N)	FALSE
 
 동물 보호소에 들어온 동물 중 아픈 동물1의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 이때 결과는 아이디 순으로 조회해주세요.
 */
+-- 코드를 입력하세요
+SELECT ANIMAL_ID,NAME from ANIMAL_INS where INTAKE_CONDITION = 'Sick' order by ANIMAL_ID ASC
