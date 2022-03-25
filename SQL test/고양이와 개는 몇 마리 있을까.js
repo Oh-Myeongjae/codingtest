1f65bce69fe300664935a9ea4d26042f6af13a9a
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2022-03-26 00:57:05 +0900

    코딩테스트 연습

--- SQL test/고양이와 개는 몇 마리 있을까.js
+++ SQL test/고양이와 개는 몇 마리 있을까.js
@@ -15,3 +15,5 @@ SEX_UPON_INTAKE     VARCHAR(N)	  FALSE
 동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회하는 SQL문을 작성해주세요.
 이때 고양이를 개보다 먼저 조회해주세요.
 */
+-- 코드를 입력하세요
+SELECT ANIMAL_TYPE, count(ANIMAL_TYPE) as 'count' from ANIMAL_INS GROUP BY ANIMAL_TYPE order by ANIMAL_TYPE ASC
