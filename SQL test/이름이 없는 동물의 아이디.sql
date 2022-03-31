4137bc07c853273b4e97a910c1ba5958593811ca
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2022-03-31 23:01:00 +0900

    코딩테스트 연습

--- SQL test/이름이 없는 동물의 아이디.sql
+++ SQL test/이름이 없는 동물의 아이디.sql
@@ -12,3 +12,5 @@ NAME	              VARCHAR(N)	    TRUE
 SEX_UPON_INTAKE	    VARCHAR(N)	    FALSE
 동물 보호소에 들어온 동물 중, 이름이 없는 채로 들어온 동물의 ID를 조회하는 SQL 문을 작성해주세요. 단, ID는 오름차순 정렬되어야 합니다.
 */
+-- 코드를 입력하세요
+SELECT ANIMAL_ID from ANIMAL_INS WHERE NAME IS NULL order by ANIMAL_ID ASC
