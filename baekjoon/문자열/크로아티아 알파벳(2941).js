27d1f031352c69c3c6a39a4328b5853f7d0fd7e5
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2022-06-09 19:05:12 +0900

    코딩테스트 연습

--- baekjoon/문자열/크로아티아 알파벳(2941).js
+++ baekjoon/문자열/크로아티아 알파벳(2941).js
@@ -12,14 +12,31 @@ nj	nj
 š	s=
 ž	z=
 예를 들어, ljes=njak은 크로아티아 알파벳 6개(lj, e, š, nj, a, k)로 이루어져 있다. 단어가 주어졌을 때, 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
-
 dž는 무조건 하나의 알파벳으로 쓰이고, d와 ž가 분리된 것으로 보지 않는다. lj와 nj도 마찬가지이다. 위 목록에 없는 알파벳은 한 글자씩 센다.
 
 입력
 첫째 줄에 최대 100글자의 단어가 주어진다. 알파벳 소문자와 '-', '='로만 이루어져 있다.
-
 단어는 크로아티아 알파벳으로 이루어져 있다. 문제 설명의 표에 나와있는 알파벳은 변경된 형태로 입력된다.
 
 출력
 입력으로 주어진 단어가 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
 */
+const fs = require('fs');
+let input = fs.readFileSync(0, 'utf8').toString().trim();
+let str = [input]
+let result = 0;
+
+let alpha = ['c=','c-','dz=','d-','lj','nj','s=','z='];
+
+for(let el of alpha){
+    str = str.map((s)=>{
+        let temp = s.split(el);
+        result += temp.length-1
+        return temp.filter((x)=>x!='')
+        }).flat()
+}
+
+str = str.join('').split('')
+result += str.length;
+
+console.log(result);
