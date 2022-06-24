e6d24ea5f39d9710b1a254f2cc0e3043c63bfa8f
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2022-06-25 00:43:27 +0900

    코딩테스트 연습

--- baekjoon/기본수학2/소수 구하기(1929).js
+++ baekjoon/기본수학2/소수 구하기(1929).js
@@ -8,3 +8,23 @@ M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오
 출력
 한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
 */
+const fs = require('fs');
+let input = fs.readFileSync(0, 'utf8').toString().trim().split(' ').map(Number);
+const func = (num)=>{
+    if(num === 1)return false;
+    let half = Math.sqrt(num);
+    
+    for(let i=2;i<=half;i++){
+        if(num%i === 0)return false;
+    }
+    
+    return true;
+}
+let A = input[0];
+let B = input[1];
+let arr = [];
+
+for(let i=A;i<=B;i++){
+    if(func(i))arr.push(i);
+}
+console.log(arr.join('\n'));
