5b3b5719feeb99bbc2aedf0a630da2cbd4ea9aa6
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2022-06-24 23:00:13 +0900

    코딩테스트 연습

--- baekjoon/기본수학2/소인수분해(11653).js
+++ baekjoon/기본수학2/소인수분해(11653).js
@@ -8,3 +8,27 @@
 출력
 N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다. N이 1인 경우 아무것도 출력하지 않는다.
 */
+const fs = require('fs');
+let input = fs.readFileSync(0, 'utf8').toString().trim();
+const func = (num)=>{
+    let half = Math.sqrt(num);
+    
+    for(let i=2;i<=half;i++){
+        if(num%i === 0)return i;
+    }
+    
+    return num;
+}
+let num = Number(input);
+if(num === 1)return;
+let arr = [];
+let half = Math.sqrt(num);
+let result = '';
+
+while(num != 1){
+    let prime = func(num);
+    arr.push(prime);
+      num /= prime;
+      }
+result = arr.sort((a,b)=>a-b).join('\n');
+console.log(result);
