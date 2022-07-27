53abd9edb28aa852e225277a04f5330046b96085
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2022-07-27 23:42:31 +0900

    코딩테스트 연습

--- level1/시저 암호(java).java
+++ level1/시저 암호(java).java
@@ -4,3 +4,20 @@
 예를 들어 "AB"는 1만큼 밀면 "BC"가 되고, 3만큼 밀면 "DE"가 됩니다. "z"는 1만큼 밀면 "a"가 됩니다. 
 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
 */
+class Solution {
+    public String solution(String s, int n) {
+       String answer = "";
+	        char[] ch = s.toCharArray();
+	  
+	        for(char c : ch) {
+	        	if(c == ' ') {
+	        		answer += c;
+	        		continue;
+	        	}
+	        	int num = c+n;        	
+	        	if((c>='a' && c<='z' && num>'z') || num>'Z') num-= 26;      	
+	        	answer += (char)num;
+	        }
+	        return answer;
+    }
+}
