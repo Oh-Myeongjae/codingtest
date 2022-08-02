e2df71ffb428f54419ae99437ec5a05bbb0e5110
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2022-08-03 01:05:41 +0900

    코딩테스트 연습

--- level1/소수 찾기(java).java
+++ level1/소수 찾기(java).java
@@ -5,3 +5,42 @@
 소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
 (1은 소수가 아닙니다.)
 */
+class Solution {
+    boolean isPrime(int number) {
+        if(number<2) return false;
+        
+         double sqrt = Math.sqrt(number);
+         
+         for(int i=2;i<=sqrt;i++) {
+             if(number%i == 0) return false;
+         }
+         return true;
+     }
+    
+    public int solution(int n) {
+          int answer = 0;
+            for(int i=1;i<=n;i++) {
+                if(isPrime(i))answer++;
+            }
+            return answer;
+    }
+}
+//----------------------------------------------------------------------------------------------------
+class Solution {
+    public int solution(int n) {
+            int answer = 0;
+            boolean[] arr = new boolean[n+1];
+            Arrays.fill(arr, true);
+            for(int i=2;i<=n;i++) {
+                if(arr[i]) {
+                    answer++;
+                    int temp = i*2;
+                    while (temp<=n) {
+                        arr[temp] = false;
+                        temp +=i;
+                    }
+                }
+            }
+            return answer;
+        }
+}
