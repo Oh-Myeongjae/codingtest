0c575852df05eb2b1fb58c7ff0e6eb41896734ba
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2022-08-04 20:55:07 +0900

    코딩테스트 연습

--- level1/약수의 합(java).java
+++ level1/약수의 합(java).java
@@ -2,3 +2,18 @@
 문제 설명
 정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
 */
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        double sqrt = Math.sqrt(n);
+         
+        for(int i=1;i<=sqrt;i++) {
+            if(n%i == 0) {
+                answer += i;
+                if(i!=sqrt) answer += n/i;   
+            }
+        }
+        
+        return answer;
+    }
+}
