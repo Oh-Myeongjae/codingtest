ec134914501f886f3dccf2d917391208722d1e9e
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2022-08-13 23:02:20 +0900

    코딩테스트 연습

--- level1/두 개 뽑아서 더하기.java
+++ level1/두 개 뽑아서 더하기.java
@@ -28,3 +28,24 @@ class Solution {
 중복과 오름차순을 해결하기 위해서 배열을 미리 만들어 놓고 인덱스로 접근하는 방식을 사용했는데
 Set을 이용하면 중복이 사라진다는걸 이용해서 set을 이용한후 정렬을 했어도 코드도 더 깔끔하고 정리가 잘될꺼 같다.
 */
+ package test;
+
+import java.util.*;
+
+public class test {
+	public int[] solution(int[] numbers) {
+        TreeSet<Integer> set = new TreeSet<Integer>();
+        for(int i=0;i<numbers.length;i++) {
+        	for(int j=0;j<numbers.length;j++) {
+            	if(j == i)continue;
+            	set.add(numbers[i]+numbers[j]);
+            }
+        }          
+        return set.stream().mapToInt(i->i).toArray();
+    }
+	
+	    public static void main(String[] args) {
+	    	test test1 = new test();
+	        System.out.println(test1.solution(new int[]{5,0,2,7}));
+	    }
+	}
