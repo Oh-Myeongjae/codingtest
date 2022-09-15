5f3f3d39f7640be4c1c1ecb395deaf4579ea594f
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2022-09-15 23:10:27 +0900

    코딩테스트 연습

--- level1/숫자 문자열과 영단어.js
+++ level1/숫자 문자열과 영단어.js
@@ -30,6 +30,7 @@ s가 의미하는 원래 숫자를 return 하도록 solution 함수를 완성해
 - 1 ≤ s의 길이 ≤ 50
 - s가 "zero" 또는 "0"으로 시작하는 경우는 주어지지 않습니다.
 - return 값이 1 이상 2,000,000,000 이하의 정수가 되는 올바른 입력만 s로 주어집니다.
+
 */
 function solution(s) {
     var answer = 0;
