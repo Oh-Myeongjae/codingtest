f28ee5f94bb8a4a82106985f60b3d8aa3cae0690
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2022-10-11 22:20:51 +0900

    코딩테스트 연습

--- 랜덤숫자뽑기.java
+++ 랜덤숫자뽑기.java
@@ -9,29 +9,28 @@ public class test {
 			map.put(0, new ArrayList<Integer>());
 			map.put(1, new ArrayList<Integer>());
 			map.put(2, new ArrayList<Integer>());
-			
+
 			boolean arr[] = new boolean[51];
 			Arrays.fill(arr, true);
-		    int count = 0;
-		    while(count<50) {
-		    	int random1 = (int)(Math.random()*3);
-		    	int random2 = (int)(Math.random()*50)+1;
-		    	ArrayList<Integer> list = map.get(random1);
-		    	if(list.size()>=17)continue;
-		    	if(arr[random2] && !list.contains(random2)) {
-		    		list.add(random2);
-		    		arr[random2] = false;
-		    		count++;
-		    	}
-		    }
+			int count = 0;
+			while(count<50) {
+					int random1 = (int)(Math.random()*3);
+					int random2 = (int)(Math.random()*50)+1;
+					ArrayList<Integer> list = map.get(random1);
+					if(list.size()>=17)continue;
+					if(arr[random2] && !list.contains(random2)) {
+						list.add(random2);
+						arr[random2] = false;
+						count++;
+					}
+				}
 			for(int s : map.keySet()) {
 				String name = s ==0 ? "우열" : (s==1 ? "원용" : "우열");
 				ArrayList<Integer> list = map.get(s);
 				Collections.sort(list);
 				System.out.println(name+" : "+map.get(s) + " , 사이즈 : "+map.get(s).size());
+				}
 			}
-			
-		}
 	    public static void main(String[] args) {
 	    	test test1 = new test();
 	        test1.solution();
