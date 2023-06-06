347580eb4d40f8e617d79c698b7dcaa8a955e2f3
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2023-06-06 23:59:31 +0900

    코딩테스트 연습

--- level2/모음 사전.js
+++ level2/모음 사전.js
@@ -31,5 +31,9 @@ function solution(word) {
 }
 /*
 (java)
-
+public int solution(String word) {
+        int answer = 0, per = 3905;
+        for(String s : word.split("")) answer += "AEIOU".indexOf(s) * (per /= 5) + 1;
+        return answer;
+    }
 */
