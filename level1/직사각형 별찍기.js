28e23bf3a669eb32d309cca2a13021e7bdece7c4
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2023-07-12 23:27:35 +0900

    코딩테스트 연습

--- level1/직사각형 별찍기.js
+++ level1/직사각형 별찍기.js
@@ -22,3 +22,17 @@ process.stdin.on('data', data => {
     }
     console.log(str)
 });
+/*
+(java)
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int x=0; x<b; x++){
+            for(int y=0; y<a; y++){
+                System.out.print("*");
+            }
+               System.out.println(); 
+        }
+    }
+*/
