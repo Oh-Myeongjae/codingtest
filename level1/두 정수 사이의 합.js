90c2c2c9d07bfe3af8800e74af4d40f57c24368c
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2023-07-15 16:02:48 +0900

    코딩테스트 연습

--- level1/두 정수 사이의 합.js
+++ level1/두 정수 사이의 합.js
@@ -16,3 +16,18 @@ function solution(a, b) {
     }
     return answer;
 }
+/*
+(java)
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        int min = Math.min(a,b);
+        int max = Math.max(a,b);
+
+        for (int i = min; i <= max; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+}
+*/
