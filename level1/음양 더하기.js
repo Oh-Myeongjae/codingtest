e50b63742e9bff0d57b894a7a60f66e413cd2f64
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2023-07-18 23:53:51 +0900

    코딩테스트 연습

--- level1/음양 더하기.js
+++ level1/음양 더하기.js
@@ -17,3 +17,13 @@ function solution(absolutes, signs) {
     }
     return answer;
 }
+/*
+(java)
+public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            answer += absolutes[i] * (signs[i] ? 1 : -1);
+        }
+        return answer;
+    }
+*/
