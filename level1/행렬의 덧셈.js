7a562432ce61843a273dc40b27c55ba5e497983a
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2023-07-23 12:31:30 +0900

    코딩테스트 연습

--- level1/행렬의 덧셈.js
+++ level1/행렬의 덧셈.js
@@ -19,3 +19,19 @@ function solution(arr1, arr2) {
     }
     return answer;
 }
+/*
+(java)
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][];
+        for (int i = 0; i < arr1.length; i++) {
+            int[] arr = new int[arr1[i].length];
+            for (int j = 0; j < arr1[i].length; j++) {
+                arr[j] = arr1[i][j] + arr2[i][j];
+            }
+            answer[i] = arr;
+        }
+        return answer;
+    }
+}
+*/
