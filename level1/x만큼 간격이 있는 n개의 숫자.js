c1b54abb358773957165efc6368c98f003c1e500
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2023-07-25 23:59:04 +0900

    코딩테스트 연습

--- level1/x만큼 간격이 있는 n개의 숫자.js
+++ level1/x만큼 간격이 있는 n개의 숫자.js
@@ -14,3 +14,14 @@ function solution(x, n) {
     }
     return answer;
 }
+/*
+(java)
+ public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        long num = x;
+        for (int i = 0; i < n; i++) {
+            answer[i] = num + (num*i);
+        }
+        return answer;
+    }
+*/
