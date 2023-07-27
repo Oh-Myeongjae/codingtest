760c21aab6822d4a4a272d7bb8489dc861aa6aef
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2023-07-27 23:43:29 +0900

    코딩테스트 연습

--- level1/부족한 금액 계산하기.js
+++ level1/부족한 금액 계산하기.js
@@ -25,3 +25,16 @@ function solution(price, money, count) {
     }
     return answer;
 }
+/*
+(java)
+public long solution(int price, int money, int count) {
+        long answer = -1;
+        long sum = 0;
+        for (int i = 1; i <= count; i++) {
+            sum += price*i;
+        }
+        answer = sum - money;
+        if(answer<0)answer=0;
+        return answer;
+    }
+*/
