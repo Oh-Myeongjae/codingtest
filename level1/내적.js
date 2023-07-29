aa03695eefff643554dd8922d71cff0ded5e752b
Oh-Myeongjae <74406343+Oh-Myeongjae@users.noreply.github.com>
2023-07-30 01:17:24 +0900

    코딩테스트 연습

--- level1/내적.js
+++ level1/내적.js
@@ -14,3 +14,15 @@ function solution(a, b) {
     }
     return answer;
 }
+/*
+(java)
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for(int i=0; i<a.length; i++){
+           answer += a[i] * b[i]; 
+        }
+        return answer;
+    }
+}
+*/
